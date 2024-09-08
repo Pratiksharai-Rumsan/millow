1c09f8c7a73c77e189f9588d51d983955a417fd8
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2024-09-08 21:06:18 +0545

    deploy the contract and log the address

--- test/Escrow.js
+++ test/Escrow.js
@@ -1,10 +1,16 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-    return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
-describe('Escrow', () => {
+describe("Escrow", () => {
+  it("returns NFT address", async () => {
+    const realState = await ethers.getContractFactory("RealEstate");
+    const escrow = await realState.deploy();
 
-})
+    //await escrow.deployed();
+    //expect(await escrow.NFT()).to.equal('0x ');
+  });
+});
