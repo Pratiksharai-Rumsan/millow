badf17b68611be2bc5b47f6339578391f54a0d64
Pratiksharai-rumsan <pratiksha.rai@rumsan.net>
2024-09-08 21:35:06 +0545

    test for Escrow contract

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,11 +6,36 @@ const tokens = (n) => {
 };
 
 describe("Escrow", () => {
-  it("returns NFT address", async () => {
-    const realState = await ethers.getContractFactory("RealEstate");
-    const escrow = await realState.deploy();
+  let buyer, seller, inspector, lender;
 
-    //await escrow.deployed();
-    //expect(await escrow.NFT()).to.equal('0x ');
+  let realState, escrow;
+  it("returns NFT address", async () => {
+    //ether return fake 20 signer
+    //setup accounts
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+    //deploy Real state
+    const RealState = await ethers.getContractFactory("RealEstate");
+    realState = await RealState.deploy();
+    //Mint the nft on behalf of the seller
+    let transaction = await realState
+      .connect(seller)
+      .mint(
+        "https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/1.json"
+      );
+    await transaction.wait();
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realState.address,
+      seller.address,
+      inspector.address,
+      lender.address
+    );
+    let result = await escrow.nftAddress();
+    expect(result).to.be.equal(realState.address);
+
+    result = await escrow.seller();
+    expect(result).to.be.equal(seller.address);
+    
   });
 });
